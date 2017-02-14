6066855ac3f82424b45b1fb678d6eaba542e0021
CasiaFan <root@iZm5e99af3f3y8u80yvm4vZ.(none)>
2017-02-14 23:06:56 +0800

    add echo mark

--- Comment_NLP_dev/sentiment_analysis_pipeline.sh
+++ Comment_NLP_dev/sentiment_analysis_pipeline.sh
@@ -2,8 +2,10 @@
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin:/home/maxfun/anaconda2/bin
 # acitavte tensorflow environment first
 source activate tensorflow
+DATE=`date +%Y-%m-%d`
 dir=/home/maxfun/maxfun_zong/yunxiu_python_data/Comment_NLP_dev
 model_path=$dir/sentiment/model/phrase_lstm_model.h5
+echo "Analysis of comment data in $DATE start!" >> $dir/sentiment_analysis.log
 if [ ! -f $model_path ]; then
     python $dir/sentiment.py --model_override --database_override > $dir/sentiment_analysis.log
 else
@@ -11,3 +13,4 @@ else
 fi
 # deactivate tensorflow
 source deactivate tensorflow
+echo "Analysis of comment data in $DATE done!" >> $dir/sentiment_analysis.log
